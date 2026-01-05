fixed broadcasting issue

src/layers/attention.py
@@ -68,10 +68,7 @@ def bigbird_block_sparse_attention(q, k, v, block_size, global_indices):
     # Add contribution from Global Keys
     attn_global_cols = F.softmax(global_col_scores * scale, dim=-1)
     g_v_flat = g_v_blocks.view(b, h, -1, d)
-    out_from_global_keys = torch.matmul(attn_global_cols, g_v_flat)
-    
-    # Reshape to match blocks
-    out_from_global_keys = out_from_global_keys.view(b, h, num_blocks, block_size, d)
+    out_from_global_keys = torch.einsum("bhnqg, bhgd -> bhnqd", attn_global_cols, g_v_flat)
     
     # Combine Local + Global Context
     final_out = out_local + out_from_global_keys

src/models/enformer_plus.py
@@ -25,9 +25,9 @@ from torch.utils.checkpoint import checkpoint_sequential
 from einops import rearrange, reduce
 from einops.layers.torch import Rearrange
 
-from src.utils.data import str_to_one_hot, seq_indices_to_one_hot
-from src.utils.config import EnformerConfig
-from src.layers.attention import BigBirdAttention
+from ..utils.data import str_to_one_hot, seq_indices_to_one_hot
+from ..utils.config import EnformerConfig
+from ..layers.attention import BigBirdAttention
 
 from transformers import PreTrainedModel
 