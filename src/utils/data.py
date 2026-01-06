d065110ad09704daebb905019cb7526072919d4f
Freddy Hong <wghong22@gmail.com>
2026-01-06 00:19:31 -0500

    Added helper functions

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,4 +1,5 @@
 """
+Helper functions adapted from enformer-pytorch/data.py from lucidrains
 Adapted from enformer/enformer-training.ipynb from google deepmind
 """
 import os
@@ -7,6 +8,60 @@ import functools
 import tensorflow as tf
 import torch
 import numpy as np
+import torch
+import torch.nn.functional as F
+
+# Helper Function from enformer-pytorch
+seq_indices_embed = torch.zeros(256).long()
+seq_indices_embed[ord('a')] = 0
+seq_indices_embed[ord('c')] = 1
+seq_indices_embed[ord('g')] = 2
+seq_indices_embed[ord('t')] = 3
+seq_indices_embed[ord('n')] = 4
+seq_indices_embed[ord('A')] = 0
+seq_indices_embed[ord('C')] = 1
+seq_indices_embed[ord('G')] = 2
+seq_indices_embed[ord('T')] = 3
+seq_indices_embed[ord('N')] = 4
+seq_indices_embed[ord('.')] = -1
+
+one_hot_embed = torch.zeros(256, 4)
+one_hot_embed[ord('a')] = torch.Tensor([1., 0., 0., 0.])
+one_hot_embed[ord('c')] = torch.Tensor([0., 1., 0., 0.])
+one_hot_embed[ord('g')] = torch.Tensor([0., 0., 1., 0.])
+one_hot_embed[ord('t')] = torch.Tensor([0., 0., 0., 1.])
+one_hot_embed[ord('n')] = torch.Tensor([0., 0., 0., 0.])
+one_hot_embed[ord('A')] = torch.Tensor([1., 0., 0., 0.])
+one_hot_embed[ord('C')] = torch.Tensor([0., 1., 0., 0.])
+one_hot_embed[ord('G')] = torch.Tensor([0., 0., 1., 0.])
+one_hot_embed[ord('T')] = torch.Tensor([0., 0., 0., 1.])
+one_hot_embed[ord('N')] = torch.Tensor([0., 0., 0., 0.])
+one_hot_embed[ord('.')] = torch.Tensor([0.25, 0.25, 0.25, 0.25])
+
+reverse_complement_map = torch.Tensor([3, 2, 1, 0, 4]).long()
+
+def torch_fromstring(seq_strs):
+    batched = not isinstance(seq_strs, str)
+    seq_strs = cast_list(seq_strs)
+    np_seq_chrs = list(map(lambda t: np.frombuffer(t.encode(), dtype = np.uint8).copy(), seq_strs))
+    seq_chrs = list(map(torch.from_numpy, np_seq_chrs))
+    return torch.stack(seq_chrs) if batched else seq_chrs[0]
+
+def str_to_seq_indices(seq_strs):
+    seq_chrs = torch_fromstring(seq_strs)
+    return seq_indices_embed[seq_chrs.long()]
+
+def str_to_one_hot(seq_strs):
+    seq_chrs = torch_fromstring(seq_strs)
+    return one_hot_embed[seq_chrs.long()]
+
+def seq_indices_to_one_hot(t, padding = -1):
+    is_padding = t == padding
+    t = t.clamp(min = 0)
+    one_hot = F.one_hot(t, num_classes = 5)
+    out = one_hot[..., :4].float()
+    out = out.masked_fill(is_padding[..., None], 0.25)
+    return out
 
 # Official DeepMind Helper Functions
 def organism_path(organism):
